50f44e3c698458d2c52de8d94a3dcfa9a819da97
Furkan Agacayaklar <ifagacayaklar@gmail.com>
2020-12-22 17:02:47 +0100

    added bootstrap and ejs boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -20,6 +21,7 @@ db.once("open", () => {
 const app = express();
 port = 3000
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
