707871bf2894b9ea04114d071bb0cfc64fb51324
Furkan Agacayaklar <ifagacayaklar@gmail.com>
2020-12-22 02:21:37 +0100

    created skeleton of app

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
+mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb://localhost:27017/camp-mapper', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -19,7 +21,10 @@ const app = express();
 port = 3000
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.status(200).render('home')
@@ -30,10 +35,39 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 })
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground)
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async(req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');   
+})
+
+
 
 
 app.listen(port, ()=> {
